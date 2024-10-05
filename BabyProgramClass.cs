cd2c328fcc2382d37b6c701562dbcdc674020c6c
Abhinayasamala2606 <abhinayasamala2606@gmail.com>
2024-10-05 12:54:05 +0530

    Changed

---
+++ BabyProgramClass.cs
@@ -0,0 +1,80 @@
+﻿namespace BabyDressShopping
+{
+    public class BabyDress
+    {
+        public int Size { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public double price { get; set; }
+    }
+    public class BabyDressUtility
+    {
+        public void AddDressToCart(BabyDress dress)
+        {
+            BabyProgramClass.DressesCart.Add(dress);  
+        }
+
+        public bool RemoveDressFromCart(string brand)
+        {
+            BabyDress dress = BabyProgramClass.DressesCart.Find(dress => dress.Brand == brand);
+            if (brand!= null)
+            {
+                BabyProgramClass.DressesCart.Remove(dress);
+                return true;
+            }
+            return false;
+        }
+    }
+    public class BabyProgramClass
+    {
+        public static List<BabyDress> DressesCart { get; set; } = new List<BabyDress>();
+
+        static void Main(string[] args)
+        {
+            BabyProgramClass program = new BabyProgramClass();
+            BabyDress dress = new BabyDress();
+            BabyDressUtility babyDressUtility = new BabyDressUtility();
+            while (true)
+            {
+                Console.WriteLine("1.AddDressToCar");
+                Console.WriteLine("2.RemoveDressFromCart");
+                Console.WriteLine("3.Exit");
+                Console.WriteLine("Enter any of above given numbers");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Enter the Size");
+                    int Size = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Color");
+                    string Color = Console.ReadLine();
+                    Console.WriteLine("Enter the Brand");
+                    string Brand = Console.ReadLine();
+                    Console.WriteLine("Enter the Price");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    babyDressUtility.AddDressToCart(dress);
+                    Console.WriteLine("Successfully added to the dress cart");
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Enter the brand of dress you want to remove");
+                    string Brand = Console.ReadLine();
+                    if (babyDressUtility.RemoveDressFromCart(Brand))
+                    {
+                        Console.WriteLine("Successfully removed from the cart");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dress not found in the cart");
+                    }
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine(" Visit Again And Thank You !!!");
+                }
+            }
+        }
+        
+    }
+}
---
+++ ProductProgram.cs
@@ -0,0 +1,62 @@
+﻿namespace Product1
+{
+    public class Product
+    {
+        public int Stock { get; set; }
+        public string Name { get; set; }
+    }
+    public class DeliveryService
+    {
+        
+        public bool PlaceOrder(Product product)
+        {
+            if (product.Stock > 0)
+            {
+                Console.WriteLine("Order placed successfully");
+                return true;
+            }
+            else
+            {
+                throw new ProductOutofStockException("Product is out of stock");
+                
+            }
+
+        }
+    }
+    public class ProductOutofStockException : Exception
+    {
+        public ProductOutofStockException(string Message)
+        {
+            
+        }
+        public override string Message => "Not found";
+
+
+    }
+    public class ProductProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine("Enter the Product Name ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Enter the Number of Stocks");
+                int Stock = int.Parse(Console.ReadLine());
+                Product product = new Product { Name=Name,Stock=Stock};
+                DeliveryService deliveryService = new DeliveryService();
+                deliveryService.PlaceOrder(product);
+            }
+            catch (ProductOutofStockException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank You for Visiting");
+            }
+            
+        }
+    }
+}
